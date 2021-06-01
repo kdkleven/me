514db73fdc0214b90883757b63314d2edbfb8622
Kyle Kleven <kylekleven8@gmail.com>
2021-05-31 23:22:19 -0500

    updated image links again

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="body">
         <h1 className="font-weight-light">Welcome</h1>        
-        <Image id="welcome" src="/img/enso.jpg" alt="welcome" roundedCircle />
+        <Image id="welcome" src="./img/enso.jpg" alt="welcome" roundedCircle />
         </div>
   );
 }
