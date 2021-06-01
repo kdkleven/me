bc5567971282eff07617b4e87bed6045b64af7bf
Kyle Kleven <kylekleven8@gmail.com>
2021-06-01 00:30:59 -0500

    Updated image links

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import { DropdownButton, Dropdown, Card } from 'react-bootstrap'
-import projects from "./projects.json"
+import Projects from "./projects.json"
 import '../Portfolio/style.css'
 
 class Project extends Component {
     state = {
-        projects
+        Projects
     };
 
     render() {
         return (
             <>
-            {this.state.projects.map(project => (
+            {this.state.Projects.map(project => (
                 <Card key={project.id} style={{ width: '18rem' }} bg="light" text="black" className="mb-1" >
-                    <Card.Img className="card-img-top" src={project.img} alt={project.name} />
+                    <Card.Img className="card-img-top" src={project.image} alt={project.name} />
                     <Card.Body className="d-flex flex-column">
                         <Card.Title>{project.name}</Card.Title>
                         <Card.Text className="card-text">
