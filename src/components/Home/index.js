2e6b3ee0a4f75a5966b68c4e40c4ecd1f91a6aa4
Kyle Kleven <kylekleven8@gmail.com>
2021-06-01 00:34:36 -0500

    Updated welcome message

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ import { Image } from 'react-bootstrap'
 function Home() {
   return (
     <div className="body">
-        <h1 className="font-weight-light">Welcome</h1>        
+        <h1 className="font-weight-light">Welcome to Kyle's Portfolio</h1>        
         <Image id="welcome" src='img/enso.jpg' alt="welcome" roundedCircle />
         </div>
   );
